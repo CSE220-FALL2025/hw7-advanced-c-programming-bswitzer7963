061e2b3af446318abe889f65603d2be3ae279a80
bswitzer7963 <theberzerker@icloud.com>
2025-11-10 15:13:45 -0500

    Beginning MCreate

--- src/hw7.c
+++ src/hw7.c
@@ -23,7 +23,24 @@ matrix_sf* transpose_mat_sf(const matrix_sf *mat) {
     return NULL;
 }
 
-matrix_sf* create_matrix_sf(char name, const char *expr) {
+matrix_sf* create_matrix_sf(char name, const char *expr) {  
+    int nrow, ncol;
+
+    int dims = sscanf(expr, " %d %d [", &name, &nrow, &ncol);
+
+    if (dims == 2) {
+        int cur = strchr(str, '[');
+        if (cur == NULL) {
+            perror("MATRIX ENTRY NULL ERROR");
+            exit(EXIT_FAILURE);
+        }
+
+
+        while((cur != '\0') && (cur != ']')) {
+            int indyrow = 
+        }
+
+        }
     return NULL;
 }
 
@@ -36,7 +53,63 @@ matrix_sf* evaluate_expr_sf(char name, char *expr, bst_sf *root) {
 }
 
 matrix_sf *execute_script_sf(char *filename) {
-   return NULL;
+    char *str = NULL;
+    size_t max_line_size = MAX_LINE_LEN;
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("OPEN SCRIPT ERROR");
+        exit(EXIT_FAILURE);
+    }
+    
+    /* Initialized bst root to NULL so that it can be pointed to on call */
+    bst_sf *root = NULL;
+    
+    while ((line = getline(&str, &max_line_size, file)) != 1) {
+        if (str == NULL) {
+            perror("BUFFER NULL ERROR");
+            exit(EXIT_FAILURE);
+        }
+        
+        char name;
+        /* If row exists, then we move to create, value irrelevant */
+        int row;
+        /* Get number of inputs recieved by sscanf to figure out if line is a creation or operation */
+        int isCreate = sscanf(str, " %c = %d ", &name, &row);
+
+        if (isCreate == 2)  {
+            char *expr = strchr(str, '=');
+            /* I dont think it can be NULL because isCreate already == 2, but just in case */
+            if ((*expr == NULL) || (*(expr + 1) == NULL)) {
+                perror("MCREATE EXPRESSION NULL ERROR");
+                exit(EXIT_FAILURE);
+            }
+            
+            /* Increment to after = */
+            expr++;
+
+            matrix_sf *mat = create_matrix_sf(name,*expr);
+            
+            /* Add to BST now */
+
+
+
+
+
+
+        }
+        /* Else either a matrix operation or an error */
+        else: {
+
+        }
+
+
+    }
+    
+    free(str);
+    fclose(file);
+    exit(EXIT_SUCCESS);
+   
+    return NULL;
 }
 
 // This is a utility function used during testing. Feel free to adapt the code to implement some of
